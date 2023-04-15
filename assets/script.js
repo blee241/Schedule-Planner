273c47346e483d02d906ffcb0892bd560b04865b
blee241 <128437170+blee241@users.noreply.github.com>
2023-04-14 18:06:36 -0700

    added feature to save text to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//Adds the current day as text in the header
+//Adds the current date as text in the header
 var currentDayEl = $('#currentDay');
 currentDayEl.text(dayjs().format('dddd, MMMM D'));
 
@@ -24,22 +24,53 @@ $(function () {
   // TODO: Add code to display the current date in the header of the page.
 });
 
-//For each of the 9 hour blocks
-for (var i = 0; i < 9; i++) {
-  var startingHour = 9;
-  var blockHour = startingHour + i;
-  var hourAdjustment = 0;
-  if (i >= 4) { //If i >= 4, then that means the time block is 1pm or later and 12 needs to be subtracted from blockHour to access the correct div id in lines 40 and 42
-    hourAdjustment = 12;
-  }
-  //If the current hour on a 24-hour clock is less than the hour on the block
-  if (dayjs().format('H') < blockHour) {
-    //Then leave the block to be its default color of green
-  } else 
-  if (dayjs().format('H') == blockHour) { //If the current hour on a 24-hour clock is equal to the hour on the block
-    $('#hour-' + (blockHour - hourAdjustment)).attr('class', 'row time-block present') //Then change the block color to red
-  } else {
-    $('#hour-' + (blockHour - hourAdjustment)).attr('class', 'row time-block past') //Else change the block to gray
+//Changes the color of the time blocks based on what time it currently is
+function changeBlockColors() {
+  for (var i = 0; i < 9; i++) { //For each of the 9 hour blocks
+    var startingHour = 9;
+    var blockHour = startingHour + i;
+    var hourAdjustment = 0;
+    if (i >= 4) { //If i >= 4, then that means the time block is 1pm or later and 12 needs to be subtracted from blockHour to access the correct div id in lines 40 and 42
+      hourAdjustment = 12;
+    }
+    if (dayjs().format('H') < blockHour) { //If the current hour on a 24-hour clock is less than the hour on the block
+      //Then leave the block to be its default color of green
+    } else 
+    if (dayjs().format('H') == blockHour) { //If the current hour on a 24-hour clock is equal to the hour on the block
+      $('#hour-' + (blockHour - hourAdjustment)).attr('class', 'row time-block present') //Then change the block color to red
+    } else {
+      $('#hour-' + (blockHour - hourAdjustment)).attr('class', 'row time-block past') //Else change the block to gray
+    }
   }
 }
+
+setInterval(changeBlockColors, 60000)
+
+//Saves text to local storage
+function saveToLocal(event) {
+  console.log(this.parentElement.children[1].value)
+  localStorage.setItem(this.parentElement.getAttribute('id'), this.parentElement.children[1].value)
+}
+
+//Creating event listeners for each save button
+var hour9SaveEl = $('#hour-9-save');
+var hour10SaveEl = $('#hour-10-save');
+var hour11SaveEl = $('#hour-11-save');
+var hour12SaveEl = $('#hour-12-save');
+var hour1SaveEl = $('#hour-1-save');
+var hour2SaveEl = $('#hour-2-save');
+var hour3SaveEl = $('#hour-3-save');
+var hour4SaveEl = $('#hour-4-save');
+var hour5SaveEl = $('#hour-5-save');
+hour9SaveEl.on('click', saveToLocal);
+hour10SaveEl.on('click', saveToLocal);
+hour11SaveEl.on('click', saveToLocal);
+hour12SaveEl.on('click', saveToLocal);
+hour1SaveEl.on('click', saveToLocal);
+hour2SaveEl.on('click', saveToLocal);
+hour3SaveEl.on('click', saveToLocal);
+hour4SaveEl.on('click', saveToLocal);
+hour5SaveEl.on('click', saveToLocal);
+
+
 
